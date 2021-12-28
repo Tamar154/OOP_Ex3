8ea95c1184b1ad57f5e86ee73d53942957f6093a
tamar-revazishvili <92332147+tamar-revazishvili@users.noreply.github.com>
2021-12-28 16:54:29 +0200

    Update Node.py

--- Ex3/src/Node.py
+++ Ex3/src/Node.py
@@ -1,16 +1,33 @@
+import random
+
+
 class Node:
     def __init__(self, pos: tuple, id: int):
+        if pos is None:
+            pos = (random.uniform(35.000, 35.30), random.uniform(32.000, 32.30))
         self.pos = pos  # tuple
         self.id = id
+        self.in_edges = []
+        self.out_edges = []
 
     def __int__(self, id: int):
         self.id = id
 
+    def add_in(self, key: int):
+        self.in_edges.append(key)
+
+    def add_out(self, key: int):
+        self.out_edges.append(key)
+
     def get_x(self):
-        return self.pos[0]
+        if self.pos is not None:
+            return self.pos[0]
+        return None
 
     def get_y(self):
-        return self.pos[1]
+        if self.pos is not None:
+            return self.pos[1]
+        return None
 
     def get_key(self) -> int:
         return self.id
@@ -21,8 +38,15 @@ class Node:
     def set_location(self, p: tuple):
         self.pos = p
 
+    #  {0: 0: |edges_out| 1 |edges in| 1, 1: 1: |edges_out| 3 |edges in| 1, 2: 2: |edges_out| 1 |edges in| 1, 3: 3: |edges_out| 0 |edges in| 2}
     def __str__(self) -> str:
-        return f"{self.id} , {self.pos}"
+        _in = ""
+        _out = ""
+        for i in self.in_edges:
+            _in += ", " + i
+        for i in self.out_edges:
+            _out += ", " + i
+        return f"{self.id}: |edges_out| {_out} |edges_in| {_in}"
 
     def __repr__(self) -> str:
-        return f"{self.id} , {self.pos}"
+        return f"{self.id}: |edges_out| {self.out_edges} |edges_in| {self.in_edges}"
