9eadf9b8496e5b93d7e26531352cda6972052c9e
tamar-revazishvili <92332147+tamar-revazishvili@users.noreply.github.com>
2021-12-28 16:52:14 +0200

    Update main.py

--- Ex3/src/main.py
+++ Ex3/src/main.py
@@ -4,23 +4,26 @@ from GraphAlgo import GraphAlgo
 
 def check():
     """
-    Graph: |V|=4 , |E|=5
-    {0: 0: |edges out| 1 |edges in| 1, 1: 1: |edges out| 3 |edges in| 1, 2: 2: |edges out| 1 |edges in| 1, 3: 3: |edges out| 0 |edges in| 2}
+    Graph: |V|=4, |E|=5
+    {0: 0: |edges_out| 1 |edges in| 1, 1: 1: |edges_out| 3 |edges in| 1, 2: 2: |edges_out| 1 |edges in| 1, 3: 3: |edges_out| 0 |edges in| 2}
     {0: 1}
     {0: 1.1, 2: 1.3, 3: 10}
     (3.4, [0, 1, 2, 3])
     (2.8, [0, 1, 3])
     (inf, [])
+    (None, inf)
     2.062180280059253 [1, 10, 7]
     17.693921758901507 [47, 46, 44, 43, 42, 41, 40, 39, 15, 16, 17, 18, 19]
     11.51061380461898 [20, 21, 32, 31, 30, 29, 14, 13, 3, 2]
     inf []
-    (7, 6.806805834715163)
-    ([1,3,4,2],3.5)
+    ([1, 9, 2, 3], 2.370613295323088)
+    (None, inf)
+    ([1, 2, 3, 4], 4.5)
     """
-    # check0()
+    check0()
     check1()
-    # check2()
+    check2()
+    check3()
 
 
 def check0():
@@ -38,17 +41,17 @@ def check0():
     g.add_edge(1, 3, 1.9)
     g.remove_edge(1, 3)
     g.add_edge(1, 3, 10)
-    print("DiGraph: ")
     print(g)  # prints the __repr__ (func output)
-    print("get_all_v()")
+    print("get_all_v:")
     print(g.get_all_v())  # prints a dict with all the graph's vertices.
-    print("all_in_edges_of_node(1)")
+    print("all in edges")
     print(g.all_in_edges_of_node(1))
-    print("all_out_edges_of_node(1)")
+    print("all out edges")
     print(g.all_out_edges_of_node(1))
-    # g_algo = GraphAlgo(g)
-    # print(g_algo.shortest_path(0, 3))
-    # g_algo.plot_graph()
+    g_algo = GraphAlgo(g)
+    print("shortest path")
+    print(g_algo.shortest_path(0, 3))
+    g_algo.plot_graph()
 
 
 def check1():
@@ -57,10 +60,12 @@ def check1():
     :return:
     """
     g_algo = GraphAlgo()  # init an empty graph - for the GraphAlgo
-    file = "../data/A0.json"
+    file = "../data/T0.json"
     g_algo.load_from_json(file)  # init a GraphAlgo from a json file
+    print("shortest path")
     print(g_algo.shortest_path(0, 3))
     print(g_algo.shortest_path(3, 1))
+    print("center")
     print(g_algo.centerPoint())
     g_algo.save_to_json(file + '_saved')
     g_algo.plot_graph()
@@ -75,6 +80,7 @@ def check2():
     g_algo.load_from_json(file)
     g_algo.get_graph().remove_edge(13, 14)
     g_algo.save_to_json(file + "_edited")
+    print("shortest path")
     dist, path = g_algo.shortest_path(1, 7)
     print(dist, path)
     dist, path = g_algo.shortest_path(47, 19)
@@ -83,6 +89,7 @@ def check2():
     print(dist, path)
     dist, path = g_algo.shortest_path(2, 20)
     print(dist, path)
+    print("tsp")
     print(g_algo.TSP([1, 2, 3]))
     g_algo.plot_graph()
 
@@ -103,7 +110,9 @@ def check3():
     g.add_edge(3, 4, 2.1)
     g.add_edge(4, 2, .5)
     g_algo = GraphAlgo(g)
+    print("center")
     print(g_algo.centerPoint())
+    print("tsp")
     print(g_algo.TSP([1, 2, 4]))
     g_algo.plot_graph()
 
